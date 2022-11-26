886f0ffb205471237c2e69e2af86b13158b48ed5
Tutorial-git <dk0540@digikull.com>
2022-11-26 17:37:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Pages/Home";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Pages/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./home.css";
+const Home = () => {
+  return (
+    <>
+      <div>
+        <h1 className="title">RIGHT STAGE TO CUSTOMIZATION</h1>
+        <p className="ptitle">Hello totally made </p>
+      </div>
+      <div>card1</div>
+      <div>card2</div>
+      <div>card3</div>
+    </>
+  );
+};
+
+export default Home;
+
+// <section className="hero-section">
+// <p>Welcome to</p>
+// <h1>Vishwa page</h1>
+// </section>
